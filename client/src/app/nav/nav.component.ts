e60a7d3b9d3ea06dcaf89a185898f007626c58b6
rvshankar20 <rvshankar20@gmail.com>
2020-12-29 17:05:31 +0530

    End of section 5

---
+++ client/src/app/nav/nav.component.ts
@@ -0,0 +1,39 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { User } from '../_models/user';
+import { AccountService } from '../_services/account.service';
+
+@Component({
+  selector: 'app-nav',
+  templateUrl: './nav.component.html',
+  styleUrls: ['./nav.component.css']
+})
+export class NavComponent implements OnInit {
+
+  model: any = {};
+  currentUser$: Observable<User> | undefined;
+
+  constructor(private accountService: AccountService) { }
+
+  ngOnInit(): void {
+    this.currentUser$ = this.accountService.CurrentUser$;
+  }
+
+  login(){
+    this.accountService.login(this.model).subscribe(response => {
+      console.log(response);
+      // this.loggedIn = true;
+    },
+    error => {
+      console.log(error);
+    }
+    );
+  }
+
+  logout(){
+    this.accountService.logout();
+    // this.loggedIn = false;
+  }
+
+}
